94456838577c52cc8d7cc92e3bc551dc7b2db442
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-10-06 01:30:46 -0700

    practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useRef, forwardRef} from "react";
+import "./App.css"
+import gsap from "gsap"
+
+const Box = forwardRef(({ children }, ref) => {
+  return <div className="box" ref={ref}>{children}</div>;
+});
+
+function Container({ children }) {
+  return <div><Box>Don't Animate Me</Box></div>;
+}
 
 function App() {
+  const box1 = useRef();
+  const box2 = useRef();
+  
+  useEffect(() => {
+    const boxes = [
+      box1.current,
+      box2.current,
+    ];
+    
+    const ctx = gsap.context(() => {
+      // Target the two specific elements we have forwarded refs to
+      gsap.to(boxes, {
+        x: 100,
+        repeat: -1,
+        repeatDelay: 1,
+        yoyo: true
+      });
+    });
+    
+    return () => ctx.revert();
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Box ref={box1}>Box</Box>
+      <Container></Container>
+      <Box ref={box2}>Box</Box>
     </div>
   );
 }
-
-export default App;
+export default App
